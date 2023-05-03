6ee061c44878ff41967dbc37466c7157fe3e9088
OlehBiletskyi <oleh.b.tech@gmail.com>
2023-05-04 00:54:38 +0300

    gh-pages
    gh-pages v2
    
    gh-paged v3

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
 import './index.css'
@@ -10,9 +10,9 @@ import store from 'store/store'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <ReduxProvider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ReduxProvider>,
 )
 
